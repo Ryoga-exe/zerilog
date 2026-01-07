da40c4c8a9dc7ae6b399670e30fbac1ece5c7366
Ryoga-exe <git@ryoga.dev>
2026-01-07 15:16:59 +0900

    feat: impl tokenizer

--- src/libs/parse/tokenizer.zig
+++ src/libs/parse/tokenizer.zig
@@ -16,8 +16,6 @@ pub const Token = struct {
         .{ "output", .keyword_output },
         .{ "inout", .keyword_inout },
         .{ "var", .keyword_var },
-        .{ "always_ff", .keyword_always_ff },
-        .{ "if_reset", .keyword_if_reset },
         .{ "else", .keyword_else },
     });
 
@@ -27,60 +25,41 @@ pub const Token = struct {
 
     pub const Tag = enum {
         invalid,
-        eof,
         identifier,
+        eof,
+        builtin,
         number_literal,
-        dot,
-        comma,
-        colon,
-        semicolon,
-        at,
-        equal,
+        doc_comment,
+        bang,
         l_paren,
         r_paren,
+        semicolon,
         l_brace,
         r_brace,
         l_bracket,
         r_bracket,
-        keyword_pub,
         keyword_module,
+        keyword_pub,
         keyword_input,
         keyword_output,
         keyword_inout,
         keyword_var,
-        keyword_always_ff,
-        keyword_if_reset,
         keyword_else,
 
         pub fn lexeme(tag: Tag) ?[]const u8 {
             return switch (tag) {
                 .invalid,
-                .eof,
                 .identifier,
-                .number_literal,
-                .keyword_pub,
-                .keyword_module,
-                .keyword_input,
-                .keyword_output,
-                .keyword_inout,
-                .keyword_var,
-                .keyword_always_ff,
-                .keyword_if_reset,
-                .keyword_else,
+                .eof,
+                .builtin,
                 => null,
 
-                .dot => ".",
-                .comma => ",",
-                .colon => ":",
-                .semicolon => ";",
-                .at => "@",
-                .equal => "=",
                 .l_paren => "(",
                 .r_paren => ")",
-                .l_brace => "{",
-                .r_brace => "}",
-                .l_bracket => "[",
-                .r_bracket => "]",
+                .semicolon => ";",
+                .keyword_module => "module",
+                .keyword_pub => "pub",
+                .keyword_else => "else",
             };
         }
 
@@ -88,146 +67,270 @@ pub const Token = struct {
             return tag.lexeme() orelse switch (tag) {
                 .invalid => "invalid token",
                 .eof => "EOF",
-                .identifier => "identifier",
-                .number_literal => "number literal",
-                .keyword_pub => "pub",
-                .keyword_module => "module",
-                .keyword_input => "input",
-                .keyword_output => "output",
-                .keyword_inout => "inout",
-                .keyword_var => "var",
-                .keyword_always_ff => "always_ff",
-                .keyword_if_reset => "if_reset",
-                .keyword_else => "else",
+                else => unreachable,
             };
         }
     };
 };
 
 pub const Tokenizer = struct {
-    buffer: []const u8,
+    buffer: [:0]const u8,
     index: usize,
 
-    pub fn init(buffer: []const u8) Tokenizer {
-        var tokenizer = Tokenizer{
+    /// For debugging purposes.
+    pub fn dump(self: *Tokenizer, token: *const Token) void {
+        std.debug.print("{s} \"{s}\"\n", .{ @tagName(token.tag), self.buffer[token.loc.start..token.loc.end] });
+    }
+
+    pub fn init(buffer: [:0]const u8) Tokenizer {
+        // Skip the UTF-8 BOM if present.
+        return .{
             .buffer = buffer,
-            .index = 0,
+            .index = if (std.mem.startsWith(u8, buffer, "\xEF\xBB\xBF")) 3 else 0,
         };
-        if (buffer.len >= 3 and std.mem.startsWith(u8, buffer, "\xEF\xBB\xBF")) {
-            tokenizer.index = 3;
-        }
-        return tokenizer;
     }
 
-    pub fn slice(self: *const Tokenizer, loc: Token.Loc) []const u8 {
-        return self.buffer[loc.start..loc.end];
-    }
+    const State = enum {
+        start,
+        expect_newline,
+        identifier,
+        builtin,
+        equal,
+        bang,
+        pipe,
+        minus,
+        asterisk,
+        slash,
+        line_comment_start,
+        line_comment,
+        doc_comment_start,
+        doc_comment,
+        int,
+        saw_at_sign,
+        invalid,
+    };
 
+    /// After this returns invalid, it will reset on the next newline, returning tokens starting from there.
+    /// An eof token will always be returned at the end.
     pub fn next(self: *Tokenizer) Token {
-        self.skipTrivia();
-
-        const start = self.index;
-        if (start >= self.buffer.len) {
-            return makeToken(.eof, start, start);
-        }
-
-        const c = self.buffer[self.index];
-        switch (c) {
-            '(' => return self.advanceSingle(.l_paren, start),
-            ')' => return self.advanceSingle(.r_paren, start),
-            '{' => return self.advanceSingle(.l_brace, start),
-            '}' => return self.advanceSingle(.r_brace, start),
-            '[' => return self.advanceSingle(.l_bracket, start),
-            ']' => return self.advanceSingle(.r_bracket, start),
-            ':' => return self.advanceSingle(.colon, start),
-            ',' => return self.advanceSingle(.comma, start),
-            '@' => return self.advanceSingle(.at, start),
-            '.' => return self.advanceSingle(.dot, start),
-            '=' => return self.advanceSingle(.equal, start),
-            ';' => return self.advanceSingle(.semicolon, start),
-            else => {},
-        }
-
-        if (isIdentifierStart(c)) {
-            return self.scanIdentifier();
-        }
-
-        if (std.ascii.isDigit(c)) {
-            return self.scanNumber();
-        }
-
-        self.index += 1;
-        return makeToken(.invalid, start, self.index);
-    }
-
-    fn skipTrivia(self: *Tokenizer) void {
-        while (self.index < self.buffer.len) {
-            const c = self.buffer[self.index];
-            switch (c) {
-                ' ', '\t', '\n', '\r' => {
+        var result: Token = .{
+            .tag = undefined,
+            .loc = .{
+                .start = self.index,
+                .end = undefined,
+            },
+        };
+        state: switch (State.start) {
+            .start => switch (self.buffer[self.index]) {
+                0 => {
+                    if (self.index == self.buffer.len) {
+                        return .{
+                            .tag = .eof,
+                            .loc = .{
+                                .start = self.index,
+                                .end = self.index,
+                            },
+                        };
+                    } else {
+                        continue :state .invalid;
+                    }
+                },
+                ' ', '\n', '\t', '\r' => {
+                    self.index += 1;
+                    result.loc.start = self.index;
+                    continue :state .start;
+                },
+                // TODO: '"'
+                // TODO: '\''
+                'a'...'z', 'A'...'Z', '_' => {
+                    result.tag = .identifier;
+                    continue :state .identifier;
+                },
+                '@' => continue :state .saw_at_sign,
+                '=' => continue :state .equal,
+                '!' => continue :state .bang,
+                '|' => continue :state .pipe,
+                '(' => {
+                    result.tag = .l_paren;
                     self.index += 1;
                 },
-                '/' => {
-                    if (self.skipLineComment()) continue;
-                    return;
+                ')' => {
+                    result.tag = .r_paren;
+                    self.index += 1;
                 },
-                else => return,
-            }
+                '[' => {
+                    result.tag = .l_bracket;
+                    self.index += 1;
+                },
+                ']' => {
+                    result.tag = .r_bracket;
+                    self.index += 1;
+                },
+                '{' => {
+                    result.tag = .l_brace;
+                    self.index += 1;
+                },
+                '}' => {
+                    result.tag = .r_brace;
+                    self.index += 1;
+                },
+                ';' => {
+                    result.tag = .semicolon;
+                    self.index += 1;
+                },
+                // TODO: '%'
+                // TODO: '*'
+                // TODO: '+'
+                // TODO: '<'
+                // TODO: '>'
+                // TODO: '^'
+                // TODO: '\\'
+                // TODO: '~'
+                // TODO: '.'
+                '-' => continue :state .minus,
+                '/' => continue :state .slash,
+                // TODO: '&'
+                '0'...'9' => {
+                    result.tag = .number_literal;
+                    self.index += 1;
+                    continue :state .int;
+                },
+                else => continue :state .invalid,
+            },
+            .expect_newline => {
+                self.index += 1;
+                switch (self.buffer[self.index]) {
+                    0 => {
+                        if (self.index == self.buffer.len) {
+                            result.tag = .invalid;
+                        } else {
+                            continue :state .invalid;
+                        }
+                    },
+                    '\n' => {
+                        self.index += 1;
+                        result.loc.start = self.index;
+                        continue :state .start;
+                    },
+                    else => continue :state .invalid,
+                }
+            },
+            .invalid => {
+                self.index += 1;
+                switch (self.buffer[self.index]) {
+                    0 => if (self.index == self.buffer.len) {
+                        result.tag = .invalid;
+                    } else {
+                        continue :state .invalid;
+                    },
+                    '\n' => result.tag = .invalid,
+                    else => continue :state .invalid,
+                }
+            },
+            .saw_at_sign => {
+                self.index += 1;
+                switch (self.buffer[self.index]) {
+                    0, '\n' => result.tag = .invalid,
+                    // TODO: '"'
+                    'a'...'z', 'A'...'Z', '_' => {
+                        result.tag = .builtin;
+                        continue :state .builtin;
+                    },
+                    else => continue :state .invalid,
+                }
+            },
+            // TODO: .ampersand
+            .asterisk => {
+                self.index += 1;
+                // TODO: switch (self.buffer[self.index])
+            },
+            .identifier => {
+                self.index += 1;
+                switch (self.buffer[self.index]) {
+                    'a'...'z', 'A'...'Z', '_', '0'...'9' => continue :state .identifier,
+                    else => {
+                        const ident = self.buffer[result.loc.start..self.index];
+                        if (Token.getKeyword(ident)) |tag| {
+                            result.tag = tag;
+                        }
+                    },
+                }
+            },
+            .builtin => {
+                self.index += 1;
+                switch (self.buffer[self.index]) {
+                    'a'...'z', 'A'...'Z', '_', '0'...'9' => continue :state .builtin,
+                    else => {},
+                }
+            },
+            // TODO: .backslash
+            .bang => {
+                self.index += 1;
+                // TODO: switch (self.buffer[self.index])
+            },
+            .pipe => {
+                self.index += 1;
+                // TODO: switch (self.buffer[self.index])
+            },
+            .equal => {
+                self.index += 1;
+                // TODO: switch (self.buffer[self.index])
+            },
+            .minus => {
+                self.index += 1;
+                // TODO: switch (self.buffer[self.index])
+            },
+            .slash => {
+                self.index += 1;
+                // TODO: switch (self.buffer[self.index])
+            },
+            .line_comment_start => {
+                self.index += 1;
+                // TODO: switch (self.buffer[self.index])
+            },
+            .doc_comment_start => {
+                self.index += 1;
+                // TODO: switch (self.buffer[self.index])
+            },
+            .line_comment => {
+                self.index += 1;
+                // TODO: switch (self.buffer[self.index])
+            },
+            .doc_comment => {
+                self.index += 1;
+                // TODO: switch (self.buffer[self.index])
+            },
+            .int => switch (self.buffer[self.index]) {
+                // TODO: '.'
+                '_', 'a'...'d', 'f'...'o', 'q'...'z', 'A'...'D', 'F'...'O', 'Q'...'Z', '0'...'9' => {
+                    self.index += 1;
+                    continue :state .int;
+                },
+                // TODO: 'e', 'E', 'p', 'P'
+                else => {},
+            },
         }
-    }
 
-    fn skipLineComment(self: *Tokenizer) bool {
-        if (self.index + 1 >= self.buffer.len) return false;
-        if (self.buffer[self.index + 1] != '/') return false;
-        self.index += 2;
-        while (self.index < self.buffer.len and self.buffer[self.index] != '\n') {
-            self.index += 1;
-        }
-        return true;
-    }
-
-    fn advanceSingle(self: *Tokenizer, tag: Token.Tag, start: usize) Token {
-        self.index += 1;
-        return makeToken(tag, start, self.index);
-    }
-
-    fn scanIdentifier(self: *Tokenizer) Token {
-        const start = self.index;
-        self.index += 1;
-        while (self.index < self.buffer.len) {
-            const ch = self.buffer[self.index];
-            if (!isIdentifierContinue(ch)) break;
-            self.index += 1;
-        }
-        const loc = Token.Loc{ .start = start, .end = self.index };
-        const ident_bytes = self.buffer[loc.start..loc.end];
-        if (Token.getKeyword(ident_bytes)) |tag| {
-            return Token{ .tag = tag, .loc = loc };
-        }
-        return Token{ .tag = .identifier, .loc = loc };
-    }
-
-    fn scanNumber(self: *Tokenizer) Token {
-        const start = self.index;
-        self.index += 1;
-        while (self.index < self.buffer.len and std.ascii.isDigit(self.buffer[self.index])) {
-            self.index += 1;
-        }
-        return makeToken(.number_literal, start, self.index);
-    }
-
-    fn makeToken(tag: Token.Tag, start: usize, end: usize) Token {
-        return .{
-            .tag = tag,
-            .loc = .{ .start = start, .end = end },
-        };
+        result.loc.end = self.index;
+        return result;
     }
 };
 
-inline fn isIdentifierStart(c: u8) bool {
-    return std.ascii.isAlphabetic(c) or c == '_';
+test "keywords" {
+    try testTokenize("pub module else", &.{ .keyword_pub, .keyword_module, .keyword_else });
 }
 
-inline fn isIdentifierContinue(c: u8) bool {
-    return std.ascii.isAlphanumeric(c) or c == '_';
+fn testTokenize(source: [:0]const u8, expected_token_tags: []const Token.Tag) !void {
+    var tokenizer = Tokenizer.init(source);
+    for (expected_token_tags) |expected_token_tag| {
+        const token = tokenizer.next();
+        try std.testing.expectEqual(expected_token_tag, token.tag);
+    }
+    // Last token should always be eof, even when the last token was invalid,
+    // in which case the tokenizer is in an invalid state, which can only be
+    // recovered by opinionated means outside the scope of this implementation.
+    const last_token = tokenizer.next();
+    try std.testing.expectEqual(Token.Tag.eof, last_token.tag);
+    try std.testing.expectEqual(source.len, last_token.loc.start);
+    try std.testing.expectEqual(source.len, last_token.loc.end);
 }
